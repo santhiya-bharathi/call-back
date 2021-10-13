e2904efa8e6571d6a534a2b01466d05621353a06
santhiya-bharathi <91201000+santhiya-bharathi@users.noreply.github.com>
2021-10-13 13:03:49 +0530

    Add files via upload

---
+++ indexcallback.js
@@ -0,0 +1,59 @@
+
+let divs = document.createElement("h1");
+ divs.setAttribute("class","happy");
+document.body.append(divs);
+
+document.querySelector(".happy").innerText= 10;
+setTimeout(()=>{
+  document.querySelector(".happy").innerText= 9;
+  setTimeout(()=>{
+  document.querySelector(".happy").innerText= 8;
+    setTimeout(()=>{
+  document.querySelector(".happy").innerText= 7;
+      setTimeout(()=>{
+  document.querySelector(".happy").innerText= 6;
+        setTimeout(()=>{
+  document.querySelector(".happy").innerText= 5;
+          setTimeout(()=>{
+  document.querySelector(".happy").innerText= 4;
+            setTimeout(()=>{
+  document.querySelector(".happy").innerText= 3;
+              setTimeout(()=>{
+  document.querySelector(".happy").innerText= 2;
+                setTimeout(()=>{
+  document.querySelector(".happy").innerText= 1;
+                  setTimeout(()=>{
+  document.querySelector(".happy").innerText= "HAPPY INDEPENDANCE DAY";
+},1000);
+},1000);
+},1000);
+},1000);
+},1000);
+},1000);
+},1000);
+},1000);
+},1000);
+},1000);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
